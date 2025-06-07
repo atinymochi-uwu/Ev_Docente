1e01ae21659b73439ed904e53b22484d7ffb7213
Javi Contreras <megustaelkesoahre@gmail.com>
2025-06-07 00:36:32 -0400

    Agregado de profesores de prueba

--- Evaluacion-Docente/src/app/features/listado/listado.component.ts
+++ Evaluacion-Docente/src/app/features/listado/listado.component.ts
@@ -15,16 +15,34 @@ export class ListadoComponent {
       averageScore: 6.1
     },
     {
+      name: ' Pedro Jara',
+      course: 'Estructura de Datos',
+      semester: '2024-2',
+      averageScore: 6.0
+    },
+    {
       name: 'María González',
       course: 'Bases de Datos',
       semester: '2025-1',
       averageScore: 5.8
     },
     {
+      name: 'Mario Casas',
+      course: 'Bases de Datos',
+      semester: '2025-1',
+      averageScore: 5.1
+    },
+    {
       name: 'Carlos Rojas',
       course: 'Sistemas Operativos',
       semester: '2025-1',
       averageScore: 6.4
+    },
+    {
+      name: 'Pablo Muñoz',
+      course: 'Sistemas Operativos',
+      semester: '2025-1',
+      averageScore: 6.7
     }
   ];
 }
